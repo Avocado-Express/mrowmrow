52d5af1fc884d70f919e97dd064ba07976a93e69
Avocado Strawberry <st3lla_programming@protonmail.com>
2026-09-02 13:53:06 +1200

    Fixed pack.sh

--- tools/pack.sh
+++ tools/pack.sh
@@ -4,7 +4,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 npx @electron/asar pack $SCRIPT_DIR/../app_unpacked $SCRIPT_DIR/../squashfs-root/resources/app.asar
-npx @electron/asar pack $SCRIPT_DIR/../obsidian_unpacked $SCRIPT_DIR/squashfs-root/resources/obsidian.asar
+npx @electron/asar pack $SCRIPT_DIR/../obsidian_unpacked $SCRIPT_DIR/../squashfs-root/resources/obsidian.asar
 
 $SCRIPT_DIR/appimagetool-x86_64.AppImage $SCRIPT_DIR/../squashfs-root $SCRIPT_DIR/../created_obsidan.AppImage
 
